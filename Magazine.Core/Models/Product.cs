088df7012cca09135ddfbeb1e1e33d21bfcc0109
one4ALL <egorrvanijked@gmail.com>
2026-03-08 18:12:05 +0300

    folder_models_create

---
+++ Magazine.Core/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magazine.Core.Models
+{
+    internal class Product
+    {
+    }
+}
